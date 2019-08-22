85eeee1821f8a30de00637e299e66333676e3086
Naoya Ono <ono.naoyaa@gmail.com>
2019-08-22 17:39:33 +0800

    Add DTO validation

--- src/blogs/dto/create-blog.dto.ts
+++ src/blogs/dto/create-blog.dto.ts
@@ -1,4 +1,10 @@
+import { IsString, IsNotEmpty } from 'class-validator'
+
 export class BlogDto {
+  @IsString()
+  @IsNotEmpty()
   readonly title: string;
+
+  @IsString()
   readonly content: string;
 }
